6053d07c994b2c0b797207d23709949d22eb42c2
yumilengjiao <1554517392@qq.com>
2025-08-07 13:02:52 +0800

    test

--- src/backend/main/main.go
+++ src/backend/main/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "backend/config"
-  "backend/handlers"
-  "github.com/gorilla/mux"
-  "log"
-  "net/http"
+	"backend/config"
+	"backend/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 var (
-  router = mux.NewRouter()
+	router = mux.NewRouter()
 )
 
 func main() {
-  log.Println(config.SessionGroups)
-  // 启动 HTTP 服务器
-  router.HandleFunc("/ws/term/{id}/{uniqId}", handlers.HandleTerminal)
-  router.HandleFunc("/ws/file/{uniqId}", handlers.HandleDirectory)
+	log.Println(config.SessionGroups)
+	// 启动 HTTP 服务器
+	router.HandleFunc("/ws/term/{id}/{uniqId}", handlers.HandleTerminal)
+	router.HandleFunc("/ws/file/{uniqId}", handlers.HandleDirectory)
 
-  err := http.ListenAndServe(":8080", router)
-  if err != nil {
-    return
-  }
+	err := http.ListenAndServe(":8977", router)
+	if err != nil {
+		return
+	}
 }
