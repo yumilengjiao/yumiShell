c990065fa750089828ad2b05550c9d2856325d43
yumilengjiao <1554517392@qq.com>
2025-08-07 16:37:41 +0800

    改变了黑暗模式按钮的位置

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="app">
     <Layout />
-    <button class="switchTheme" @click="switchTheme">切换主题</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Layout from "@renderer/layout/index.vue"
-import { useThemeStore } from "@renderer/stores/useThemeStore"
 
-const themeStore = useThemeStore()
-
-const switchTheme = () => {
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark')
-    themeStore.theme = 'light'
-  } else {
-    document.documentElement.classList.add('dark')
-    themeStore.theme = 'dark'
-  }
-}
 </script>
 
 <style scoped lang="scss">
@@ -27,11 +14,5 @@ const switchTheme = () => {
   border-radius: 10px;
   height: 100vh;
   width: 100vw;
-
-  .switchTheme {
-    position: fixed;
-    top: 20px;
-    right: 20px;
-  }
 }
 </style>
